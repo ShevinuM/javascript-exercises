92cdc05c7ccd80709a73bc5080410c1934301f13
ShevinuM <nsmcooray@mun.ca>
2023-09-21 19:10:30 -0230

    Completed the exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return people.map((person) => ({person:person, age: (person.yearOfDeath || currentYear) - person.yearOfBirth}))
+                 .reduce((oldest, person) => {
+                    return (oldest.age > person.age) ? oldest : person;
+                 }, {age:0}).person;
 };
 
 // Do not edit below this line
